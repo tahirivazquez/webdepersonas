53476e57d73cda291238c0eca32dded7b4d12646
tahirivazquez <tahiwdev@gmail.com>
2023-10-01 20:55:32 -0300

    dark mode

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//Función del darkmode
 
+function toggleDarkMode() {
+    const currentTheme = document.querySelector("body").getAttribute("data-bs-theme");
+    if (currentTheme === "light") {
+        localStorage.setItem("darkMode", "dark");
+        document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+        document.querySelector("body").setAttribute("data-bs-theme", "light");
+    }
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkMode = localStorage.getItem("darkMode");
+    document.querySelector("body").setAttribute("data-bs-theme", darkMode || "light");
+    const darkModeButton = document.getElementById("dark-mode");
+
+    if (darkModeButton) {
+        darkModeButton.addEventListener('click', () => {
+            toggleDarkMode();
+        })
+    }
+
+});
